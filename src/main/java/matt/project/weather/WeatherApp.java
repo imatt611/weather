d62290aa5a4712426e00c856f502f0a701bacaa5
matt.cochrane <matt.cochrane@cayuse.com>
2018-11-06 07:24:09 -0800

    Build in Spring Boot, test/implement variable-allowing output string.

---
+++ src/main/java/matt/project/weather/WeatherApp.java
@@ -0,0 +1,23 @@
+package matt.project.weather;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApp implements ApplicationRunner {
+
+    @SuppressWarnings("StaticMethodOnlyUsedInOneClass")
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WeatherApp.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args)
+    {
+        System.out.println(
+                "At the location $CITY_NAME, the temperature is $TEMPERATURE, the timezone is $TIMEZONE, and the elevation is $ELEVATION.");
+    }
+}
---
+++ src/test/java/matt/project/weather/WeatherAppTest.java
@@ -0,0 +1,49 @@
+package matt.project.weather;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@SuppressWarnings("resource")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherAppTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams()
+    {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams()
+    {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void generatesWeatherDescription()
+    {
+        // Must trigger #main from test to ensure it feeds System#out into the stream under test
+        WeatherApp.main(new String[]{});
+        Pattern weatherDescriptionPattern = Pattern.compile(
+                "At the location .*, the temperature is .*, the timezone is .*, and the elevation is .*\\.");
+        assertThat(weatherDescriptionPattern.matcher(outContent.toString()).find(), is(true));
+    }
+}
