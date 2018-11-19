ab1da7c49206646527a5182897ddb30e3b2d4db1
matt.cochrane <matt.cochrane@cayuse.com>
2018-11-19 08:28:12 -0800

    WIP Initialize failing test, barely begin implementation

---
+++ src/main/java/matt/project/weather/GoogleTimeZoneService.java
@@ -0,0 +1,11 @@
+package matt.project.weather;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@NoArgsConstructor
+public class GoogleTimeZoneService {
+}
