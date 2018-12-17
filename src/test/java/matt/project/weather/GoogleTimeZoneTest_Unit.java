f3cc04430313ae0d0f4dd7a661000c3500f3712f
Matthew Inman Cochrane <matt.cochrane@cayuse.com>
2018-12-17 08:47:16 -0800

    Apply unit test of deserialization to GoogleTimeZoneData
    TODO
    - Address duplicate code

---
+++ src/test/java/matt/project/weather/GoogleTimeZoneTest_Unit.java
@@ -0,0 +1,28 @@
+package matt.project.weather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class GoogleTimeZoneTest_Unit {
+
+    @Test
+    public void deserializesResults() throws Exception
+    {
+        // expect
+        GoogleTimeZoneData expectedTimeZoneData = new GoogleTimeZoneData();
+        expectedTimeZoneData.setTimeZoneName("Eastern Daylight Time");
+
+        // when
+        ObjectMapper mapper = new ObjectMapper();
+        URL src = getClass().getResource("/sampleResponse_googleTimeZone.json");
+        GoogleTimeZoneData timeZoneData = mapper.readValue(src, GoogleTimeZoneData.class);
+
+        assertThat(timeZoneData, equalTo(expectedTimeZoneData));
+    }
+
+}
